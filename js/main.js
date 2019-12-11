88e23fdfcf046ef18b3c2d83359a8e4781e06d64
tekerene <54807911+tekerene@users.noreply.github.com>
2019-12-11 14:42:54 +0100

    initialazation of the repository

---
+++ js/main.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+
+	"use strict";
+	$('#slider-carousel').caroufredsel({
+		responsive:true,
+		width:'100%',
+		circular:true,
+		scroll:{
+			items:1,
+			duration:500,
+			pauseOnHover:true
+		},
+		auto:true,
+		items:
+		{
+			visible:{
+				min:1,
+				max:1
+			},
+			height:"variable"
+		},
+		pagination:{
+			container:".sliderpager",
+			pageAnchorBuilder:false
+		}
+	});
+		$(window).scroll(function(){
+			var top = $(window).scrollTop();
+			if(top>=60){
+				$("header").addClass('secondary');
+			}
+			else 
+				if($("header").hasClass('secondary')){
+					$("header").removeClass('secondary');
+				}
+		});
+});
